cb1747759b995a3bc71e3e42a7d7cd06067ef45a
Snir4game <sniramsalem1995@gmail.com>
2024-01-06 15:12:46 +0200

    Todo List

--- App.js
+++ App.js
@@ -1,11 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import  React from 'react';
+import { KeyboardAvoidingView, Platform, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
+import Task from './components/Task'; 
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+
+        {/* Title  */}
+      <View style={styles.taskWrapper}>
+        <Text style={styles.sectionTitle}>Today's tasks</Text>
+
+        <View style={styles.items}>
+        {/* Tasks */}
+        <Task Text={'Task One'}/>
+        <Task Text={'Task Two'}/>
+        <Task Text={'Task Three'}/>
+        <Task Text={'Task Four'}/>
+
+        </View>
+
+      </View>
+      {/* Write a Task */}
+      <KeyboardAvoidingView
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+      style={styles.WriteTaskWrapper}
+      >
+        <TextInput style={styles.input} placeholder={'Write a Task'} />
+        <TouchableOpacity>
+          <View style={styles.addWrapper}>
+            <Text style={styles.addText}>+</Text>
+          </View>
+        </TouchableOpacity>
+      </KeyboardAvoidingView>
     </View>
   );
 }
@@ -13,8 +40,18 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#E8EAED',
+
   },
+  taskWrapper:{
+    paddingTop:80,
+    paddingHorizontal:20
+  },
+  sectionTitle:{
+    fontSize:24,
+    fontWeight:'bold',
+  },
+  items:{
+    marginTop:30,
+  }
 });
